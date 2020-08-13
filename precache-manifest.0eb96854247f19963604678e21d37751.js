8f97010a15d5e68fb409e9c6f3f341fced461877
Emily Barrera <emily.barrera.b@gmail.com>
2020-08-12 20:15:15 -0600

    Updates

---
+++ precache-manifest.0eb96854247f19963604678e21d37751.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a21f0897446a93cf2ab230bca1830625",
+    "url": "/Beginner-English-Lessons/index.html"
+  },
+  {
+    "revision": "a9b3e9039d9b29bc73eb",
+    "url": "/Beginner-English-Lessons/static/css/main.e7bc6927.chunk.css"
+  },
+  {
+    "revision": "7a8020e392bae8d8f4a6",
+    "url": "/Beginner-English-Lessons/static/js/2.2496dd87.chunk.js"
+  },
+  {
+    "revision": "eaa98556b572aaaafcc6d8cf367f0f70",
+    "url": "/Beginner-English-Lessons/static/js/2.2496dd87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b3e9039d9b29bc73eb",
+    "url": "/Beginner-English-Lessons/static/js/main.c689378c.chunk.js"
+  },
+  {
+    "revision": "e1c1e16c9728897f7ef3",
+    "url": "/Beginner-English-Lessons/static/js/runtime-main.a3e621dc.js"
+  },
+  {
+    "revision": "cff7015fb3b7a8fcf691cffd781dfece",
+    "url": "/Beginner-English-Lessons/static/media/apple.cff7015f.png"
+  },
+  {
+    "revision": "fac7e654558135b7d2bd439747072939",
+    "url": "/Beginner-English-Lessons/static/media/food.fac7e654.mov"
+  },
+  {
+    "revision": "857e8fe90bd70bf5ad523592feb1405f",
+    "url": "/Beginner-English-Lessons/static/media/introduction-2.857e8fe9.mp4"
+  }
+]);
